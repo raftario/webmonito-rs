4a372b356842851e45e4c584e2433692dd57ca79
Raphaël Thériault <raphael_theriault@outlook.com>
2019-06-13 16:22:00 -0400

    Do not keep web client and web transport alive to reduce memory usage

--- src/lib.rs
+++ src/lib.rs
@@ -25,9 +25,6 @@ pub fn run(matches: clap::ArgMatches) {
     let verbose = config.verbose.unwrap_or(false);
     verbose_println(verbose, "  Config parsed.");
 
-    let client = reqwest::Client::new();
-    let mut transport = SendmailTransport::new();
-
     let mut list = urls::hash_list(&config.urls);
     for (k, v) in list.clone().iter() {
         let hash = urls::compare(&client, k, v).unwrap_or_else(|e| {
@@ -41,59 +38,69 @@ pub fn run(matches: clap::ArgMatches) {
     }
 
     loop {
-        println!("- Checking for updates...");
-        for (k, v) in list.clone().iter() {
-            let hash = urls::compare(&client, k, v).unwrap_or_else(|e| {
-                eprintln!("Request error for {}: {}", &k, e.to_string());
-                None
-            });
-            if let Some(r) = hash {
-                println!("  Page at {} has changed", &k);
-                list.insert(k.clone(), r);
+        {
+            let client = reqwest::Client::new();
+            let mut transport = SendmailTransport::new();
 
-                if let Some(p) = &config.pings {
-                    for ping in p {
-                        let response = pings::send(
-                            &client,
-                            &ping.url,
-                            match &ping.content {
-                                Some(c) => c,
-                                None => "",
-                            },
-                            k,
-                        )
-                        .unwrap_or_else(|e| {
-                            eprintln!("Ping error for {}: {}", &ping.url, e.to_string());
-                            String::new()
-                        });
+            println!("- Checking for updates...");
+            for (k, v) in list.clone().iter() {
+                let hash = urls::compare(&client, k, v).unwrap_or_else(|e| {
+                    eprintln!("Request error for {}: {}", &k, e.to_string());
+                    None
+                });
+                verbose_println(verbose, &format!("  New hash for {} obtained.", &k));
 
-                        verbose_println(
-                            verbose,
-                            &format!("  Ping sent to {}. Response is {}", &ping.url, response),
-                        );
+                if let Some(r) = hash {
+                    println!("  Page at {} has changed", &k);
+                    list.insert(k.clone(), r);
+
+                    if let Some(p) = &config.pings {
+                        for ping in p {
+                            let response = pings::send(
+                                &client,
+                                &ping.url,
+                                match &ping.content {
+                                    Some(c) => c,
+                                    None => "",
+                                },
+                                k,
+                            )
+                            .unwrap_or_else(|e| {
+                                eprintln!("Ping error for {}: {}", &ping.url, e.to_string());
+                                String::new()
+                            });
+
+                            verbose_println(
+                                verbose,
+                                &format!("  Ping sent to {}. Response is {}", &ping.url, response),
+                            );
+                        }
                     }
-                }
 
-                if let Some(e) = &config.emails {
-                    for email in e {
-                        emails::send(
-                            &mut transport,
-                            match &config.sender {
-                                Some(s) => s,
-                                None => "wbmrs@wbmrs.com",
-                            },
-                            &email.address,
-                            match &email.content {
-                                Some(c) => c,
-                                None => "",
-                            },
-                            k,
-                        )
-                        .unwrap_or_else(|e| {
-                            eprintln!("Email error for {}: {}", &email.address, e.to_string());
-                        });
+                    if let Some(e) = &config.emails {
+                        for email in e {
+                            emails::send(
+                                &mut transport,
+                                match &config.sender {
+                                    Some(s) => s,
+                                    None => "wbmrs@wbmrs.com",
+                                },
+                                &email.address,
+                                match &email.content {
+                                    Some(c) => c,
+                                    None => "",
+                                },
+                                k,
+                            )
+                            .unwrap_or_else(|e| {
+                                eprintln!("Email error for {}: {}", &email.address, e.to_string());
+                            });
 
-                        verbose_println(verbose, &format!("  Email sent to {}.", &email.address));
+                            verbose_println(
+                                verbose,
+                                &format!("  Email sent to {}.", &email.address),
+                            );
+                        }
                     }
                 }
             }
